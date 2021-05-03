a7db2ac22cd4fa09accc49d7e211165e4e4b73d5
Chris Johnson <chrisjohnson00@hotmail.com>
2021-05-02 19:22:40 -0700

    wip

--- app.py
+++ app.py
@@ -10,11 +10,11 @@ url = os.environ.get('AMQP_URL', 'amqp://guest:guest@localhost/%2f')
 params = pika.URLParameters(url)
 params.socket_timeout = 5
 
-connection = pika.BlockingConnection(params)  # Connect to CloudAMQP
+connection = pika.BlockingConnection(params)  # Connect to AMQP_URL
 channel = connection.channel()  # start a channel
 channel.queue_declare(queue='pdfprocess')  # Declare a queue
 # send a message
 
 channel.basic_publish(exchange='', routing_key='pdfprocess', body='User information')
-print("[x] Message sent to consumer")
+print("[x] Message sent to consumer", flush=True)
 connection.close()
