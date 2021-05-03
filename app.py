185969d7d671c012fd435451eccc683405775d8b
Chris Johnson <chrisjohnson00@hotmail.com>
2021-05-02 18:57:43 -0700

    wip

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import logging
 
 logging.basicConfig()
 
-# Parse CLODUAMQP_URL (fallback to localhost)
-url = os.environ.get('CLOUDAMQP_URL', 'amqp://guest:guest@localhost/%2f')
+# Parse AMQP_URL (fallback to localhost)
+url = os.environ.get('AMQP_URL', 'amqp://guest:guest@localhost/%2f')
 params = pika.URLParameters(url)
 params.socket_timeout = 5
 
